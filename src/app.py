72167f177f2cff51e606e2c44cb86694827fc50a
lmgarciag02 <lmgarciag02@gmail.com>
2025-05-16 03:48:21 +0000

    inicio

--- src/app.py
+++ src/app.py
@@ -1,4 +1,22 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+model = load(open("../models/random_forest_regression.sav", "rb"))
+
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        val1 = float(request.form["CRIM"])
+        val2 = float(request.form["RM"])
+        val3 = float(request.form["DIS"])
+        val4 = float(request.form["LSTAT"])
+        val5 = float(request.form["AGE"])
+
+        data = [[val1, val2, val3, val4, val5]]
+        prediction = int(model.predict(data)[0])
+
+    else:
+        prediction = 0
+    return render_template("index.html", prediction = prediction)
